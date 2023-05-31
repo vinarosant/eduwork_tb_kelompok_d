85244d0921314cae563deb815d38c495cf4f9a73
vinarosant <vinarosant@gmail.com>
2023-05-31 18:34:18 +0700

    update logo

--- about.php
+++ about.php
@@ -3,19 +3,16 @@
 <?php include "head.html" ?>
 
    <body>
-       
-    <!-- Preloader Start -->
-    <div id="preloader-active">
+   <div id="preloader-active">
         <div class="preloader d-flex align-items-center justify-content-center">
             <div class="preloader-inner position-relative">
                 <div class="preloader-circle"></div>
                 <div class="preloader-img pere-text">
-                    <img src="assets/img/logo/logo.png" alt="">
+                    <img src="assets/img/atas.png" alt="">
                 </div>
             </div>
         </div>
     </div>
-    <!-- Preloader Start -->
 </div>
 <?php include "header.php" ?>
 
