a9d1b54c54094e3f28ff06b7fea9af0505bd6d25
feki marinda <200631100063@student.ac.id>
2023-06-05 19:32:15 +0700

    edit

--- about.php
+++ about.php
@@ -1,9 +1,11 @@
-<?php include 'admin/koneksi.php'; ?>
-
 <!doctype html>
 <html class="no-js" lang="zxx">
 <?php include "head.html" ?>
 <style>
+.p{
+  text-align: justify;
+}
+
   .team {
   background: white;
   padding: 60px 0;
@@ -76,9 +78,6 @@
         <div class="about-area">
             <div class="container">
                     <!-- Hot Aimated News Tittle-->
-
-                    <?php include 'trending.php'; ?>
-
                     <div class="row">
                         <div class="col-lg-12">
                             <div class="trending-tittle">
@@ -106,12 +105,11 @@
                                             <h3>About ED News</h3>
                                         </div>
                                         <div class="about-prea">
-                                            <p class="about-pera1 mb-25">ED NEWS adalah media digital terpopuler dan terbesar di Indonesia dengan konsep breaking news yang menyajikan informasi peristiwa terkini dan gaya hidup.</p>
-                                            <p class="about-pera1 mb-25">Semua berita dapat anda akses di ED NEWS secara cepat dan akurat, mulai dari olahraga sampai pendidikan semua disajikan dalam ED NEWS</p>
-                                            <p class="about-pera1 mb-25">
+                                            <p class="about-pera1 mb-25 p">
                                             Ed News adalah sebuah platform berita daring yang menyediakan informasi terkini dan terpercaya dalam berbagai kategori topik. Dibangun dengan tujuan untuk memberikan wawasan mendalam dan beragam kepada para pembaca, Ed News menyajikan berita-berita terbaru yang relevan, menarik, dan dapat diandalkan.
-                                            Kami mengerti bahwa kebutuhan informasi setiap individu berbeda-beda. Oleh karena itu, Ed News menawarkan berbagai kategori topik yang luas, mencakup berbagai bidang dan minat, sehingga setiap pembaca dapat menemukan berita yang sesuai dengan minat dan kebutuhan mereka.
-                                            </p>
+                                            Kami mengerti bahwa kebutuhan informasi setiap individu berbeda-beda. Oleh karena itu, Ed News menawarkan berbagai kategori topik yang luas, mencakup berbagai bidang dan minat, sehingga setiap pembaca dapat menemukan berita yang sesuai dengan minat dan kebutuhan mereka. <br>
+                                            Tujuannya adalah memberikan layanan kepada para pembaca harian ednews di tempat-tempat yang sulit dijangkau oleh jaringan distribusi ednews. Dengan hadirnya ednews Online, para pembaca harian ednews terutama di Indonesia bagian timur dan di luar negeri dapat menikmati harian ednews hari itu juga, tidak perlu menunggu beberapa hari seperti biasanya  
+                                          </p>
                                         </div>
                                     
                                     <!-- ======= Team Section ======= -->
