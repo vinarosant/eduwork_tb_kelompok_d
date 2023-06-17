fed55572b523d9b42c4b8e2395d8dc68d9308055
faazin <radenfauzi1312@gmail.com>
2023-06-17 22:21:12 +0700

    add like

---
+++ addlike.php
@@ -0,0 +1,16 @@
+<?php
+$id_berita = $_GET['id'];
+
+include 'admin/koneksi.php';
+
+$querylike = mysqli_query($koneksi, "SELECT * FROM `like` WHERE id_berita = '$id_berita'");
+
+if (mysqli_num_rows($querylike) > 0) {
+    $upquery = mysqli_query($koneksi, "UPDATE `like` SET `jumlah_like` = `jumlah_like` + 1 WHERE `id_berita` = '$id_berita'");
+} else {
+    $inquery= mysqli_query($koneksi, "INSERT INTO `like` (id_berita, `jumlah_like`) VALUES ('$id_berita', 1)");
+}
+
+http_response_code(200);
+?>
+
---
+++ getlikecount.php
@@ -0,0 +1,15 @@
+<?php
+$id_berita = $_GET['id'];
+
+include 'admin/koneksi.php';
+
+$querylike = mysqli_query($koneksi, "SELECT * FROM `like` WHERE `id_berita` = '$id_berita'");
+
+if (mysqli_num_rows($querylike) > 0) {
+    $row = mysqli_fetch_assoc($querylike);
+    $jumlah_like = $row['jumlah_like'];
+    echo $jumlah_like;
+} else {
+    echo 0;
+}
+?>
